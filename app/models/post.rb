c9a20c57b06a3c028ff08aa3cd762043772efad2
Oleksandr Bratashov <abratashov@gmail.com>
2026-09-19 17:07:16 +0300

    Add HotWire live updates

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,11 @@
 class Post < ApplicationRecord
+  has_many :comments, dependent: :destroy
+
+  broadcasts_to :replace_posts
+
+  private
+
+  def replace_posts
+    broadcast_replace_to "posts", target: "posts", partial: "posts/posts"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   resources :comments
   resources :posts
-# Model Context Protocol
-post "/mcp", to: "mcp#handle"
-get  "/mcp", to: "mcp#handle"
+
+  # Model Context Protocol
+  post "/mcp", to: "mcp#handle"
+  get  "/mcp", to: "mcp#handle"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -15,5 +16,5 @@ get  "/mcp", to: "mcp#handle"
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "posts#index"
 end
